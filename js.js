1bdd5cfc165f23c22304b3920eab93084d9ac3c7
Seijno <seijnovannieuwkoop@gmail.com>
2023-12-04 10:16:35 +0100

    import

---
+++ js.js
@@ -0,0 +1,9 @@
+document.getElementById("openMenu").addEventListener("click", openMenu);
+document.getElementById("closeMenu").addEventListener("click", closeMenu);
+
+function openMenu() {
+document.getElementById("sideMenu").style.width = "50vw";
+}
+
+function closeMenu() {
+document.getElementById("sideMenu").style.width = "0";}
